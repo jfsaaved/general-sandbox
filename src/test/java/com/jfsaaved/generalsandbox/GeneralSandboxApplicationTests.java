fd3ab9bd3876f6ad0659a9aba2816ac03c49b8a5
jfsaaved <julian.felipe.saavedra@gmail.com>
2020-01-02 14:05:43 -0500

    RabbitMQ playaround

---
+++ src/test/java/com/jfsaaved/generalsandbox/GeneralSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfsaaved.generalsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/jfsaaved/generalsandbox/GeneralSandboxRunnerTest.java
@@ -0,0 +1,7 @@
+package com.jfsaaved.generalsandbox;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeneralSandboxRunnerTest {
+
+}
